f6adbee9e43cc196966101a508f85469f6be272b
Nishant Kumar <nishant.kumar9500@gmail.com>
2025-08-07 20:16:54 -0400

    deleted files as its wrong

--- agents/sql_generator_agent.py
+++
@@ -1,5 +0,0 @@
-class SQLGeneratorAgent:
-    def generate_sql(self, xml_dict):
-        # This is where the XML is transformed into SQL.
-        # Example output (you'll need to implement a real transformation logic):
-        return "SELECT * FROM dataset.table WHERE condition = TRUE;"
